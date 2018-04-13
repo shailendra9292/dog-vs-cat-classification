09283a5f93052b52f2543a33fae1a2e6f119afc1
shailendra9292 <37996516+shailendra9292@users.noreply.github.com>
2018-04-13 10:40:10 +0530

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout , Flatten
+from keras.layers import Conv2D, MaxPooling2D
+ 
+model = Sequential()
+
+# first layer
+model.add(Conv2D(32,(3,3), activation='relu', input_shape=(64,64,3)))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#flatten
+model.add(Flatten())
+
+#full connection
+model.add(Dense(128,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
+
+#compilation
+model.compile(optimizer='adam', loss = 'binary_crossentropy', metrics=['accuracy'])
+
+# data augmentation
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('C:/Users/MY-FAMILY/Anaconda3/Machine Learning/Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/dataset/training_set',
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('C:/Users/MY-FAMILY/Anaconda3/Machine Learning/Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/dataset/test_set',
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
+
+model.fit_generator(training_set,
+                    steps_per_epoch=8000,
+                    epochs=20,
+                    validation_data=test_set ,
+                    validation_steps=2000)

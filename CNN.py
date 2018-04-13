c10aeba06b9278bdec0f1a14ef945cc853900912
shailendra9292 <37996516+shailendra9292@users.noreply.github.com>
2018-04-13 14:12:07 +0530

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -41,12 +41,12 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory('C:/Users/MY-FAMILY/Anaconda3/Machine Learning/Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/dataset/training_set',
+training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size=(64, 64),
                                                  batch_size=32,
                                                  class_mode='binary')
 
-test_set = test_datagen.flow_from_directory('C:/Users/MY-FAMILY/Anaconda3/Machine Learning/Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/dataset/test_set',
+test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size=(64, 64),
                                             batch_size=32,
                                             class_mode='binary')
